d7963a35184ee401f0e16512bce2447d53724c8e
Max <max21011999@mail.ru>
2021-04-25 03:08:33 +0300

    +second realization

--- main.js
+++ main.js
@@ -1,20 +1,40 @@
 'use strict';
 
-let createHash = () => {
-    return createHash;
+// Реализация 1
+let createHash1 = () => {
+    return createHash1;
 }
-
-createHash.hashMap = new Map();
-createHash.set = function (key, value) {
+createHash1.hashMap = new Map();
+createHash1.set = function (key, value) {
     this.hashMap.set(key, value);
     return this;
 };
-createHash.remove = function (key) {
+createHash1.remove = function (key) {
     this.hashMap.delete(key);
     return this;
 };
-createHash.getValues = function () {
+createHash1.getValues = function () {
     return Object.fromEntries(this.hashMap.entries());
 };
 
-console.log(createHash().set('1', 2).set('3', 4).remove('1').getValues());
+// Реализация 2
+const createHash2 = () => {
+    this.hashMap = {
+        map: new Map(),
+        set: function (key, value) {
+            this.map.set(key, value);
+            return this;
+        },
+        remove: function (key) {
+            this.map.delete(key);
+            return this;
+        },
+        getValues: function () {
+            return Object.fromEntries(this.map.entries());
+        }
+    }
+    return this.hashMap;
+}
+
+console.log(createHash1().set('1', 2).set('3', 4).remove('1').getValues());
+console.log(createHash2().set('1', 2).set('3', 4).remove('1').getValues());
